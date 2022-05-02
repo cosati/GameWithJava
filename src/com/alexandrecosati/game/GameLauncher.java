af0fd22c97bb0aacd3f94b06a7cd3a0a9e9f89d1
Alexandre Cosati <alexandre_cosati@epam.com>
2022-05-03 00:54:48 +0200

    First Commit - Game Window

---
+++ src/com/alexandrecosati/game/GameLauncher.java
@@ -0,0 +1,13 @@
+package com.alexandrecosati.game;
+
+public class GameLauncher {
+
+    public GameLauncher() {
+        new Window();
+    }
+
+    public static void main(String... args) {
+        new GameLauncher();
+    }
+
+}
